c453d1340032400d8f48a6d77b39c999ca716a0f
Thomas Kranitsas <kranitsasthomas@gmail.com>
2018-04-19 20:54:13 +0300

    Fix static server

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/allure-data'));
+app.use(express.static(__dirname + '/allure-report'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/allure-data/index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/allure-report/index.html')));
 
 // Start the app by listening on the default
 app.listen(process.env.PORT || 8080);
