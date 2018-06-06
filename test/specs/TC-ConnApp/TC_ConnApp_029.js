f270d9c459216bcdd67f3ec44be254bef7c39727
Vladimir <vov4ik_lv@yahoo.com>
2018-06-06 15:35:43 +0300

    Fixed 029

--- test/specs/TC-ConnApp/TC_ConnApp_029.js
+++ test/specs/TC-ConnApp/TC_ConnApp_029.js
@@ -9,10 +9,10 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
 
     before(function () {
       browser.loginToConnApp('admin');
+
       browser.waitForVisible('#wrapper-main .content-pane section > div > div > section > div > div.container > div > div > div:nth-child(2) .row');
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(3) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(3)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
       browser.pause(10000);//** wait the data loaded.
     });
 
@@ -33,19 +33,19 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
     const projectStatusListPaused = `${projectStatusList}` + ' .ProjectStatus.status-paused';
 
     it('Check the options available for "Status" filtering', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li';
       const items = browser.getText(s);
       assert.equal(items.length, 8);
-      const filters = ['All projects', 'Active', 'Draft', 'In review', 'Reviewed', 'Completed', 'Cancelled', 'Paused'];
+      const filters = ['All Projects', 'Active', 'Draft', 'In review', 'Reviewed', 'Completed', 'Cancelled', 'Paused'];
       for (var i = 0; i < items.length; i++) {
         items[i].should.be.equal(filters[i]);
       }
     });
 
     it('Check Draft filter function', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(3) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(3)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
+      browser.waitForVisible(`${s}.E7SY3s`);
       browser.pause(10000);
       browser.elements(projectStatusListInReview).value.should.be.empty;
       browser.elements(projectStatusListReviewed).value.should.be.empty;
@@ -56,9 +56,9 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
       browser.elements(projectStatusListDraft).value.should.not.be.empty;
     });
     it('Check In review filter function', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(4) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(4)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
+      browser.waitForVisible(`${s}.E7SY3s`);
       browser.pause(10000);
       browser.elements(projectStatusListDraft).value.should.be.empty;
       browser.elements(projectStatusListReviewed).value.should.be.empty;
@@ -69,9 +69,9 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
       browser.elements(projectStatusListInReview).value.should.not.be.empty;
     });
     it('Check Reviewed filter function', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(5) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(5)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
+      browser.waitForVisible(`${s}.E7SY3s`);
       browser.pause(10000);
       browser.elements(projectStatusListDraft).value.should.be.empty;
       browser.elements(projectStatusListInReview).value.should.be.empty;
@@ -83,9 +83,9 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
 
     });
     it('Check Completed filter function', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(6) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(6)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
+      browser.waitForVisible(`${s}.E7SY3s`);
       browser.pause(10000);
       browser.elements(projectStatusListDraft).value.should.be.empty;
       browser.elements(projectStatusListInReview).value.should.be.empty;
@@ -96,9 +96,9 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
       browser.elements(projectStatusListCompleted).value.should.not.be.empty;
     });
     it('Check Cancelled filter function', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(7) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(7)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
+      browser.waitForVisible(`${s}.E7SY3s`);
       browser.pause(10000);
       browser.elements(projectStatusListDraft).value.should.be.empty;
       browser.elements(projectStatusListInReview).value.should.be.empty;
@@ -109,9 +109,9 @@ describe('#TC_ConnApp_029 - Connect Application : E2E Dashboard Functionalities'
       browser.elements(projectStatusListCancelled).value.should.not.be.empty;
     });
     it('Check Paused filter function', () => {
-      const s = '#wrapper-main .list-nav-container .left-wrapper > li:nth-child(8) > a';
+      const s = '#wrapper-main .list-nav-container .left-wrapper > div > :nth-child(2) > ul > li:nth-child(8)';
       browser.click(s);
-      browser.waitForVisible(`${s}.active`);
+      browser.waitForVisible(`${s}.E7SY3s`);
       browser.pause(10000);
       browser.elements(projectStatusListDraft).value.should.be.empty;
       browser.elements(projectStatusListInReview).value.should.be.empty;
